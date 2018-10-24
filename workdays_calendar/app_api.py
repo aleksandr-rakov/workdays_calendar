00aa27caee8b1659bc988fe5092bfe64c3cf0c51
Aleksandr Rakov <aleksandr.rakov@gmail.com>
2018-10-24 14:34:16 +0300

    refactor_app_api

--- workdays_calendar/app_api.py
+++ workdays_calendar/app_api.py
@@ -4,31 +4,30 @@ from workdays_calendar.days_calendar import get_day_int
 from workdays_calendar.tags import HOLIDAY_TAG
 import datetime
 
-def get_holiday_tag(db):
-    return db[TAGS_COLLECTION].find_one({'name': HOLIDAY_TAG})
+def get_tag(db,tag_name):
+    return db[TAGS_COLLECTION].find_one({'name': tag_name})
 
-def is_holiday(db,day,holiday_tag=None):
-    if holiday_tag is None:
-        holiday_tag=get_holiday_tag(db)
-    if not holiday_tag:
+def day_has_tag(db,day,tag):
+    if not tag:
         return False
 
     day_int=get_day_int(day)
     stored_day=db[CALENDAR_COLLECTION].find_one({'day_int': day_int})
     if stored_day:
-        return str(holiday_tag['_id']) in stored_day['tags']
+        return str(tag['_id']) in stored_day['tags']
     return False
 
 def is_holiday_today(db):
-    return is_holiday(db,datetime.date.today())
+    holiday_tag=get_tag(db,HOLIDAY_TAG)
+    return day_has_tag(db,datetime.date.today(),holiday_tag)
 
 def get_workdays_interval(db,start,num_days):
-    holiday_tag=get_holiday_tag(db)
+    holiday_tag=get_tag(db,HOLIDAY_TAG)
 
     holidays_used=False
     total_days=0
 
-    while is_holiday(db,start,holiday_tag):
+    while day_has_tag(db,start,holiday_tag):
         start+=datetime.timedelta(days=1)
         holidays_used=True
         total_days+=1
@@ -38,7 +37,7 @@ def get_workdays_interval(db,start,num_days):
     while drive_days>0:
         day+=datetime.timedelta(days=1)
         total_days+=1
-        if is_holiday(db,day,holiday_tag):
+        if day_has_tag(db,day,holiday_tag):
             holidays_used=True
         else:
             drive_days-=1
